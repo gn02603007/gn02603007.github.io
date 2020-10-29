8c929de9c63dba5dc3175e0842184186c54d5254
Jhe-An, Lin <gn02603007@gmail.com>
2020-10-29 17:21:27 +0800

    youtube player 0.0.1

---
+++ YoutubePlayer/precache-manifest.46065c2348b6f6f556f3dfb97f1484a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "1db48dc1017272ecffb6",
+    "url": "./static/js/main.1db48dc1.chunk.js"
+  },
+  {
+    "revision": "6ff4a9ae61d190dda0cc",
+    "url": "./static/js/2.6ff4a9ae.chunk.js"
+  },
+  {
+    "revision": "1db48dc1017272ecffb6",
+    "url": "./static/css/main.f82da72f.chunk.css"
+  },
+  {
+    "revision": "6ff4a9ae61d190dda0cc",
+    "url": "./static/css/2.19c70849.chunk.css"
+  },
+  {
+    "revision": "9716e540df4dfa4f2caccbf91881d186",
+    "url": "./index.html"
+  }
+];
